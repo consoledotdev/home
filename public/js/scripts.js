d1e1b8e06e0b34ce75509d5db694c6bb3b607298
morekid <daniele.dematteis@gmail.com>
2021-01-23 17:21:40 +0100

    initial responsive menu setup

---
+++ public/js/scripts.js
@@ -0,0 +1,8 @@
+let toggleMenu = (e) => {
+    let el = document.getElementById("menu-toggle");
+    if (el.classList.contains("is-active")) {
+        el.classList.remove("is-active");
+    } else {
+        el.classList.add("is-active");
+    }
+};
