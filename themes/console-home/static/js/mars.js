cdce89bd812f6e44063bad0dd5eb7668917bbc3b
morekid <daniele.dematteis@gmail.com>
2021-06-07 12:55:25 +0200

    better TOC scrolling

--- themes/console-home/static/js/mars.js
+++ themes/console-home/static/js/mars.js
@@ -21,21 +21,51 @@ let updateDTNTable = (e) => {
 
 let computeTOCPos = (() => {
     let style = {};
+    let reachedBottom = false;
     let compute = function () {
+        let handleIntersect = (intersections) => {
+            intersections.forEach(function (i) {
+                if (i.isIntersecting) {
+                    reachedBottom = true;
+                }
+                if (!i.isIntersecting) {
+                    let rootTop = i.rootBounds.top;
+                    let elTop = i.boundingClientRect.top;
+                    if (rootTop < elTop) {
+                        reachedBottom = false;
+                    }
+                }
+            });
+        };
+        let observer = new IntersectionObserver(handleIntersect, {
+            root: null,
+            rootMargin: "0% 0% -95%",
+            threshold: 0,
+        });
+        observer.observe(lastBlock);
+
         let aside = document.querySelector("[data-aside]");
         let TOC = document.querySelector("[data-toc]");
+        let lastBlock = document.querySelector("#wrapper-10");
         let midPoint = parseInt(window.innerHeight / 2);
         let asidePos = aside.getBoundingClientRect().top;
         let TOCHeightHalf = parseInt(TOC.offsetHeight / 2);
         let diff = asidePos - midPoint + TOCHeightHalf;
 
         if (diff < 0) {
-            style.marginTop = diff * -1 + "px";
+            style.position = "fixed";
+            style.top = midPoint - TOCHeightHalf + "px";
+            if (reachedBottom) {
+                let pos = midPoint - TOCHeightHalf + lastBlock.getBoundingClientRect().top;
+                style.top = pos + "px";
+            }
         } else {
-            style.marginTop = null;
+            style.position = null;
+            style.top = null;
         }
 
-        TOC.style.marginTop = style.marginTop;
+        TOC.style.position = style.position;
+        TOC.style.top = style.top;
     };
     compute();
     window.addEventListener("scroll", compute);
