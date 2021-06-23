0d788bbe5dbb4735401a86caf00602480c9064b5
morekid <daniele.dematteis@gmail.com>
2021-06-23 17:09:17 +0200

    fixes gltich in filtering functionality for betas

--- themes/console-home/static/js/betas.js
+++ themes/console-home/static/js/betas.js
@@ -1,21 +1,15 @@
-// overrides generic feature.js filtering method 
+// overrides generic feature.js filtering method
 filtering._filter = (cards, filterValues) => {
-    // for each card checks if its categories data attribute
-    // has any match for the active checkboxes values
-    // If there's no match it hides the card, otherwise it shows it
-    // Weekly pick filter will show/hide all weekly pick records
-    // regardles if other categories are matched
     let count = 0;
     for (let card of cards) {
-        let categories = card.dataset.categories;
+        let taxonomy = card.dataset.taxonomy;
         let match = false;
 
-        if (categories.indexOf("weekly-pick") >= 0 && !(filterValues.indexOf("weekly-pick") >= 0)) {
+        if (taxonomy.indexOf("weekly-pick") >= 0 && !(filterValues.indexOf("weekly-pick") >= 0)) {
             // no match
-        }
-        else {
+        } else {
             for (value of filterValues) {
-                match = categories.indexOf(value) >= 0 && true;
+                match = taxonomy.indexOf(value) >= 0 && true;
                 if (match) break;
             }
         }
@@ -28,4 +22,4 @@ filtering._filter = (cards, filterValues) => {
         }
     }
     return count;
-}
+};
