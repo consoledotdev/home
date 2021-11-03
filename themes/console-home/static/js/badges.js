b4ac7090b805698d083dd20e192eb613129bfdbc
morekid <daniele.dematteis@gmail.com>
2021-11-03 15:26:20 +0100

    implement background change on scroll

--- themes/console-home/static/js/badges.js
+++ themes/console-home/static/js/badges.js
@@ -153,3 +153,51 @@ let handleCopyEmbedCode = (() => {
         console.error("Couldn't copy.");
     });
 })();
+
+let handleScrollAppearance = (() => {
+    let handleIntersect = (intersections) => {
+        intersections.forEach(function (i) {
+            if ("checkpointSelectionCriteria" in i.target.dataset) {
+                if (i.isIntersecting || (!i.isIntersecting && i.boundingClientRect.top < 0)) {
+                    document.body.classList.add("checkpoint-selection-criteria");
+                }
+                if (!i.isIntersecting) {
+                    if (i.boundingClientRect.top > 0) document.body.classList.remove("checkpoint-selection-criteria");
+                }
+            }
+        });
+    };
+
+    let observed1 = ["data-checkpoint-selection-criteria"].map((str) => {
+        return document.querySelector("[" + str + "]");
+    });
+
+    let observer1 = new IntersectionObserver(handleIntersect, {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0,
+    });
+
+    observed1.forEach((el) => {
+        observer1.observe(el);
+    });
+
+    let onScrollEnd = () => {
+        observed1.forEach((el) => {
+            observer1.unobserve(el);
+            observer1.observe(el);
+        });
+    };
+
+    var isScrolling;
+    window.addEventListener(
+        "scroll",
+        () => {
+            window.clearTimeout(isScrolling);
+            isScrolling = setTimeout(() => {
+                onScrollEnd();
+            }, 50);
+        },
+        false
+    );
+})();
