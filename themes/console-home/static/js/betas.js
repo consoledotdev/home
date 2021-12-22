bd70be2f499a8b723042b2f2d86b616c5f0a2de6
morekid <daniele.dematteis@gmail.com>
2021-12-22 18:49:09 +0100

    comment out previous filtering code

--- themes/console-home/static/js/betas.js
+++ themes/console-home/static/js/betas.js
@@ -1,28 +1,28 @@
-// overrides generic feature.js filtering method
-filtering._filter = (cards, filterValues) => {
-    let count = 0;
-    for (let card of cards) {
-        let taxonomy = card.dataset.taxonomy;
-        let match = false;
+// // overrides generic feature.js filtering method
+// filtering._filter = (cards, filterValues) => {
+//     let count = 0;
+//     for (let card of cards) {
+//         let taxonomy = card.dataset.taxonomy;
+//         let match = false;
 
-        if (taxonomy.indexOf("weekly-pick") >= 0 && !(filterValues.indexOf("weekly-pick") >= 0)) {
-            // no match
-        } else {
-            for (value of filterValues) {
-                match = taxonomy.indexOf(value) >= 0 && true;
-                if (match) break;
-            }
-        }
+//         if (taxonomy.indexOf("weekly-pick") >= 0 && !(filterValues.indexOf("weekly-pick") >= 0)) {
+//             // no match
+//         } else {
+//             for (value of filterValues) {
+//                 match = taxonomy.indexOf(value) >= 0 && true;
+//                 if (match) break;
+//             }
+//         }
 
-        if (match) {
-            card.classList.remove("is-hidden");
-            count++;
-        } else {
-            card.classList.add("is-hidden");
-        }
-    }
-    return count;
-};
+//         if (match) {
+//             card.classList.remove("is-hidden");
+//             count++;
+//         } else {
+//             card.classList.add("is-hidden");
+//         }
+//     }
+//     return count;
+// };
 
 // first sorting when the page is loaded to order data from json
 sortItems("name");
--- themes/console-home/static/js/tools.js
+++
@@ -1,9 +0,0 @@
-// first sorting when the page is loaded to order data from json
-sortItems("name");
-sortItems("date");
-
-// first grouping when the page is loaded
-groupItems();
-
-// first count when the page is loaded
-filtering.selectedAllChecks();
