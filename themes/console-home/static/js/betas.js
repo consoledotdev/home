5d0f35050b754a13a9560b5380b64060667f9588
morekid <daniele.dematteis@gmail.com>
2021-12-22 18:50:22 +0100

    remove unused code

--- themes/console-home/static/js/betas.js
+++ themes/console-home/static/js/betas.js
@@ -1,29 +1,3 @@
-// // overrides generic feature.js filtering method
-// filtering._filter = (cards, filterValues) => {
-//     let count = 0;
-//     for (let card of cards) {
-//         let taxonomy = card.dataset.taxonomy;
-//         let match = false;
-
-//         if (taxonomy.indexOf("weekly-pick") >= 0 && !(filterValues.indexOf("weekly-pick") >= 0)) {
-//             // no match
-//         } else {
-//             for (value of filterValues) {
-//                 match = taxonomy.indexOf(value) >= 0 && true;
-//                 if (match) break;
-//             }
-//         }
-
-//         if (match) {
-//             card.classList.remove("is-hidden");
-//             count++;
-//         } else {
-//             card.classList.add("is-hidden");
-//         }
-//     }
-//     return count;
-// };
-
 Filter.prototype.filter = function () {
     let activeFilterValues = this.getActiveFilterValues();
     for (let section of this.sections.public().getFilterables()) {
