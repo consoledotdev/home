cd566714054d6202a1fd16a8450e84d12a3b86c2
morekid <daniele.dematteis@gmail.com>
2022-05-31 19:22:10 +0200

    different approach to horizon lines

--- themes/console-home/assets/js/zero.js
+++ themes/console-home/assets/js/zero.js
@@ -1,6 +1,8 @@
-let easeInCubic = (x) => {
-    return x * x * x;
-};
+function animate(time) {
+    requestAnimationFrame(animate);
+    TWEEN.update(time);
+}
+requestAnimationFrame(animate);
 
 class HorizonLines {
     constructor(el) {
@@ -8,6 +10,7 @@ class HorizonLines {
         this.el = el;
         this.linesAmt = this.el.dataset.horizonLines;
         this.lines = this.generateLines();
+        this.animateLines();
     }
 
     bindFuncs() {}
@@ -18,20 +21,30 @@ class HorizonLines {
             let line = this.generateLine(i);
             this.el.append(line.div);
             lines.push(line);
+            let ghostLine = this.generateLine(i, true);
+            this.el.append(ghostLine.div);
+            lines.push(ghostLine);
         }
         return lines;
     }
 
-    generateLine(i) {
+    generateLine(i, isGhost) {
         const div = document.createElement("div");
         div.classList.add("horizon-line");
+        if (isGhost) div.classList.add("horizon-line-ghost");
 
         let line = {};
+        line.ghost = isGhost || false;
         line.div = div;
-        let maxHeight = 100 / this.linesAmt;
-        line.height = maxHeight * 0.75;
-        line.scale = 1 - (1 / this.linesAmt) * i; // adjust this for different scaling proportions
-        line.margin = maxHeight * (1 - (1 / this.linesAmt) * i) * 0.2; // adjust this for different margin proportions
+        let size = (100 / this.linesAmt) * 0.7;
+
+        if (!isGhost) {
+            line.height = size;
+            line.scale = 1 - (1 / this.linesAmt) * i;
+        } else {
+            line.height = size * (1 - (1 / this.linesAmt) * i) * 0.75;
+            line.scale = 1;
+        }
 
         this.setLine(line);
 
@@ -41,7 +54,22 @@ class HorizonLines {
     setLine(line) {
         line.div.style.height = line.height + "%";
         line.div.style.transform = "scaleY(" + line.scale + ")";
-        line.div.style.marginBottom = line.margin + "%";
+    }
+
+    animateLines() {
+        let toAnimate = this.lines.filter((l) => l.ghost == false);
+        toAnimate.forEach((line, idx) => {
+            let move = { scale: line.scale, translate: 0 };
+            let scaleEnd = 0;
+            if (idx < toAnimate.length - 1) scaleEnd = toAnimate[idx + 1].scale;
+            const tween = new TWEEN.Tween(move)
+                .to({ scale: scaleEnd, translate: 100 }, 1000)
+                .repeat(Infinity)
+                .onUpdate(() => {
+                    line.div.style.transform = "scaleY(" + move.scale + ") translateY(" + (move.translate * 2) / move.scale + "%)";
+                })
+                .start();
+        });
     }
 }
 
@@ -50,5 +78,4 @@ document.addEventListener("DOMContentLoaded", (e) => {
     document.querySelectorAll("[data-horizon-lines]").forEach((el) => {
         horizonLinesBlocks.push(new HorizonLines(el));
     });
-    console.log(horizonLinesBlocks);
 });
