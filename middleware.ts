96cc0223ca216ea2f4ca68faab010f4a6a733709
David Mytton <david@mytton.net>
2024-04-24 10:23:40 -0400

    Update Arcjet

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,11 @@
-import arcjet, { createMiddleware } from "@arcjet/next";
+import arcjet, { createMiddleware, shield } from "@arcjet/next";
 export const config = {
     // matcher tells Next.js which routes to run the middleware on
     matcher: ["/((?!_next/static|_next/image|favicon.ico|favicon.png|img).*)"],
 };
-const aj = arcjet({ key: process.env.ARCJET_KEY!, rules: [] });
+const aj = arcjet({
+    key: process.env.ARCJET_KEY!,
+    rules: [shield({ mode: "LIVE" })],
+});
 
 export default createMiddleware(aj);
