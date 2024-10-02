4d1621c7238934e464a00a23faa9bc42c2b57105
David Mytton <david@arcjet.com>
2024-10-02 12:36:38 +0100

    Fix broken Next.js

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,19 @@
-import arcjet, { createMiddleware, shield } from "@arcjet/next";
+import arcjet, { createMiddleware, shield, detectBot } from "@arcjet/next";
 export const config = {
     // matcher tells Next.js which routes to run the middleware on
     matcher: ["/((?!_next/static|_next/image|favicon.ico|favicon.png|img).*)"],
 };
 const aj = arcjet({
     key: process.env.ARCJET_KEY!,
-    rules: [shield({ mode: "LIVE" })],
+    rules: [
+        shield({ mode: "LIVE" }),
+        detectBot({
+            mode: "DRY_RUN", // will block requests. Use "DRY_RUN" to log only
+            // Block all bots except search engine crawlers. See the full list of bots
+            // for other options: https://arcjet.com/bot-list
+            allow: ["CATEGORY:SEARCH_ENGINE", "CATEGORY:FEEDFETCHER", "CATEGORY:PREVIEW", "CATEGORY:SOCIAL"],
+        }),
+    ],
 });
 
 export default createMiddleware(aj);
