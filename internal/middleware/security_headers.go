dcca5303346e510eff563030b5279aca1caf3205
David Mytton <david@console.dev>
2026-05-18 23:30:06 +0000

    fix: add plausible to csp

--- internal/middleware/security_headers.go
+++ internal/middleware/security_headers.go
@@ -5,7 +5,7 @@ import "net/http"
 func SecurityHeadersMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Security-Policy",
-			"default-src 'self'; script-src 'self' https://plausible.io; frame-src https://embed.podcasts.apple.com; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'")
+			"default-src 'self'; script-src 'self' https://plausible.io; connect-src 'self' https://plausible.io; frame-src https://embed.podcasts.apple.com; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'")
 		w.Header().Set("X-Frame-Options", "DENY")
 		w.Header().Set("X-Content-Type-Options", "nosniff")
 		w.Header().Set("Strict-Transport-Security", "max-age=63072000; includeSubDomains")
